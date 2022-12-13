b8cdbad8cf9a1b4bd72df43c8e00f573bd55ac66
saadsaleem <saad.sal748@gmail.com>
2022-12-13 23:40:50 +0500

    commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,20 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:dailymed_search/controllers/daily-med-provider.dart';
+import 'package:dailymed_search/services/api-service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:dailymed_search/main.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('Test Api Result',(WidgetTester test)async {
+   await test.pumpWidget(MyApp());
+    ApiService apiService = ApiService();
+    List result = [];
+   result = apiService.fetchMedList(BuildContext) as List;
+    expect(result, List);
   });
 }
